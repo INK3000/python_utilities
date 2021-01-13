3732f7c8ade11de2e38a14036c1d76c1167aa1bf
ILYA <ilya@krivenok.com>
2021-01-13 15:14:04 +0200

    Добавлена функция translit

---
+++ python_utilities.py
@@ -0,0 +1,59 @@
+def translit(text):
+    '''
+    Функция транслитерации с латинского алфавита в кирилицу. Строчные и заглавные буквы сохраняют свое состояние.
+
+    Пример:
+    translit('Строка') вернет 'Stroka'
+
+    Пример:
+    l = ['С', 'т', 'р', 'о', 'к', 'а']
+    translit(l) вернет 'Stroka'
+
+
+    :param text: может быть строкой, списком или кортежем
+    :return: независимо от типа принятой переменной возвращает строку, транслитерированную по словарю ru_lt_dict
+    '''
+    ru_lt_dict = {
+        'а': 'a',
+        'б': 'b',
+        'в': 'v',
+        'г': 'g',
+        'д': 'd',
+        'е': 'e',
+        'ё': 'jo',
+        'ж': 'zh',
+        'з': 'z',
+        'и': 'i',
+        'й': 'j',
+        'к': 'k',
+        'л': 'l',
+        'м': 'm',
+        'н': 'n',
+        'о': 'o',
+        'п': 'p',
+        'р': 'r',
+        'с': 's',
+        'т': 't',
+        'у': 'u',
+        'ф': 'f',
+        'х': 'h',
+        'ц': 'c',
+        'ч': 'ch',
+        'ш': 'sh',
+        'щ': 'sch',
+        'ъ': '',
+        'ы': 'y',
+        'ь': '',
+        'э': 'je',
+        'ю': 'ju',
+        'я': 'ja'
+    }
+
+    pre_result = list(text)
+    for idx, char in enumerate(pre_result):
+        char_is_upper = char.isupper()
+        char = char.lower()
+        if char in ru_lt_dict:
+            pre_result[idx] = char_is_upper and ru_lt_dict[char].upper() or ru_lt_dict[char]
+    result = ''.join(pre_result)
+    return result
